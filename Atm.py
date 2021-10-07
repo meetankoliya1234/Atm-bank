94093faeff3512befa414088c3fa36438a580796
Meet2008 <72910517+meetankoliya1234@users.noreply.github.com>
2021-10-07 22:11:19 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,13 @@
+class atm(object):
+    def __init__(self,pin,password,cvv):
+        self.pin = pin
+        self.password = password
+        self.cvv = cvv
+    def insert(self):
+        print("The number is 234")
+    def security(self):
+        print("The number is 546")
+    def sec2(self):
+        print("The number is 345")
+Meet = atm("1234","148","567")
+Meet.sec2()
